a4807c62aba15f772036b0d7edf9f9ed7ac5ed86
taskalataminfo2026 <taska.latam.info@gmail.com>
2025-11-08 13:14:32 -0500

    Test de database v1

--- cmd/api/constants/general.go
+++ cmd/api/constants/general.go
@@ -4,3 +4,11 @@ const (
 	DateFormatISO      = "2006-01-02"
 	DateTimeFormatFull = "2006-01-02 15:04:05"
 )
+
+const (
+	TypeUserUser       = "user"
+	TypeUserTasker     = "tasker"
+	TypeUserManager    = "manager"
+	TypeUserAdmin      = "admin"
+	TypeUserSuperAdmin = "superadmin"
+)
